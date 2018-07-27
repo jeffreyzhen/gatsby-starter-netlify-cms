6b4e4434249af06cae7ff5b4b26d177fed455328
Jeffrey Zhen <jeffrey@echobind.com>
2018-07-27 18:44:26 -0400

    markdown for content

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -21,7 +21,7 @@ const AboutPagePreview = ({ entry, widgetFor, getAsset }) => (
         image: getAsset(entry.getIn(["data", "section1", "image", "image"])),
         alt: entry.getIn(["data", "section1", "image", "alt"])
       },
-      content: entry.getIn(["data", "section1", "content"]),
+      content: widgetFor("content"),
       quote: {
         quote: entry.getIn(["data", "section1", "quote", "quote"]),
         name: entry.getIn(["data", "section1", "quote", "name"]),
