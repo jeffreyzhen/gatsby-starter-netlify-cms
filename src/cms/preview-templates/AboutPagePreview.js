9738096fdd7b0f13ffc2ae216a183532c1da560a
Jeffrey Zhen <jeffrey@echobind.com>
2018-07-27 14:49:09 -0400

    added about preview tempalte

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -1,19 +1,41 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { AboutPageTemplate } from '../../templates/about-page'
+import React from "react";
+import PropTypes from "prop-types";
+import { AboutPageTemplate } from "../../templates/about-page";
 
 const AboutPagePreview = ({ entry, widgetFor }) => (
   <AboutPageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
+    title={entry.getIn(["data", "title"])}
+    intro={{
+      image: {
+        image: getAsset(entry.getIn(["data", "intro", "image", "image"])),
+        alt: entry.getIn(["data", "intro", "image", "alt"])
+      },
+      blurb1: entry.getIn(["data", "intro", "blurb1"]),
+      blurb2: entry.getIn(["data", "intro", "blurb2"]),
+      blurb3: entry.getIn(["data", "intro", "blurb3"]),
+      blurb4: entry.getIn(["data", "intro", "blurb4"])
+    }}
+    main={{
+      heading: entry.getIn(["data", "main", "heading"]),
+      image: {
+        image: getAsset(entry.getIn(["data", "main", "image", "image"])),
+        alt: entry.getIn(["data", "main", "image", "alt"])
+      },
+      description: entry.getIn(["data", "main", "description"]),
+      quote: {
+        quote: entry.getIn(["data", "main", "quote", "quote"]),
+        name: entry.getIn(["data", "main", "quote", "name"]),
+        title: entry.getIn(["data", "main", "quote", "title"])
+      }
+    }}
   />
-)
+);
 
 AboutPagePreview.propTypes = {
   entry: PropTypes.shape({
-    getIn: PropTypes.func,
+    getIn: PropTypes.func
   }),
-  widgetFor: PropTypes.func,
-}
+  widgetFor: PropTypes.func
+};
 
-export default AboutPagePreview
+export default AboutPagePreview;
