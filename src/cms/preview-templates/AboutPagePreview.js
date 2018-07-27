ac209197af0b39a87f010225b973801b3ff7ab9d
Jeffrey Zhen <jeffrey@echobind.com>
2018-07-27 19:21:44 -0400

    changed content to body

--- src/cms/preview-templates/AboutPagePreview.js
+++ src/cms/preview-templates/AboutPagePreview.js
@@ -21,7 +21,7 @@ const AboutPagePreview = ({ entry, widgetFor, getAsset }) => (
         image: getAsset(entry.getIn(["data", "section1", "image", "image"])),
         alt: entry.getIn(["data", "section1", "image", "alt"])
       },
-      content: widgetFor("content"),
+      content: widgetFor("body"),
       quote: {
         quote: entry.getIn(["data", "section1", "quote", "quote"]),
         name: entry.getIn(["data", "section1", "quote", "name"]),
